85a558ecdb8ca2ec37f454cd94ecf546c614d25a
Daniel Olivier <dan_olivier@hotmail.com>
2017-10-14 11:29:52 -0400

    Modify BlockChain, embed Block and BlockIterator classes

--- OrbitCore/BlockChain.h
+++ OrbitCore/BlockChain.h
@@ -5,98 +5,95 @@
 #include <atomic>
 
 //-----------------------------------------------------------------------------
-template < class T, int BlockSize > struct BlockChain;
-
-//-----------------------------------------------------------------------------
-template < class T, int Size > struct Block
+template < class T, unsigned BlockSize >
+struct BlockChain
 {
-    Block(BlockChain<T, Size>* a_Chain, Block<T, Size>* a_Prev)
-        : m_Prev(a_Prev)
-        , m_Next(nullptr)
-        , m_Chain(a_Chain)
-        , m_Size(0)
+    struct Block
     {
-    }
-
-    ~Block()
-    {
-    }
-
-    void Add(const T & a_Item)
-    {
-        if (m_Size == Size)
+        Block(BlockChain* a_Chain, Block* a_Prev)
+            : m_Prev(a_Prev)
+            , m_Next(nullptr)
+            , m_Chain(a_Chain)
+            , m_Size(0)
         {
-            if( m_Next == nullptr )
-            {
-                m_Next = new Block<T, Size>(m_Chain, this);
-            }
-
-            m_Chain->m_Current = m_Next;
-            ++m_Chain->m_NumBlocks;
-            m_Next->Add(a_Item);
-            return;
         }
 
-        assert( m_Size < Size );
-        m_Data[m_Size] = a_Item;
-        ++m_Size;
-        ++m_Chain->m_NumItems;
-    }
-
-    Block<T, Size>*       m_Prev;
-    Block<T, Size>*       m_Next;
-    T                     m_Data[Size];
-    BlockChain<T, Size>*  m_Chain;
-    std::atomic<int>      m_Size;
-};
-
-//-----------------------------------------------------------------------------
-template<class T, int BlockSize> struct BlockIterator
-{
-    BlockIterator(Block<T, BlockSize>* a_Block) : m_Block(a_Block)
-    {
-        m_Index = (m_Block && (m_Block->m_Size > 0)) ? 0 : -1;
-    }
-
-    T& operator*()
-    {
-        return m_Block->m_Data[m_Index];
-    }
-
-    bool operator!=(const BlockIterator & other) const
-    {
-        bool returnValue = m_Index != other.m_Index;
-        return returnValue;
-    }
-
-    BlockIterator& operator++()
-    {
-        if (++m_Index == m_Block->m_Size)
+        ~Block()
         {
-            if (m_Block->m_Next && m_Block->m_Next->m_Size > 0)
-            {
-                m_Index = 0;
-                m_Block = m_Block->m_Next;
-            }
-            else
-            {
-                m_Index = -1;
-            }
         }
 
-        return *this;
-    }
+        void Add(const T & a_Item)
+        {
+            if (m_Size == BlockSize)
+            {
+                if (m_Next == nullptr)
+                {
+                    m_Next = new Block(m_Chain, this);
+                }
 
-    Block<T, BlockSize>* m_Block;
-    int m_Index;
-};
+                m_Chain->m_Current = m_Next;
+                ++m_Chain->m_NumBlocks;
+                m_Next->Add(a_Item);
+                return;
+            }
 
-//-----------------------------------------------------------------------------
-template < class T, int BlockSize > struct BlockChain
-{
+            assert(m_Size < BlockSize);
+            m_Data[m_Size] = a_Item;
+            ++m_Size;
+            ++m_Chain->m_NumItems;
+        }
+
+        Block*              m_Prev;
+        Block*              m_Next;
+        T                   m_Data[BlockSize];
+        BlockChain*         m_Chain;
+        std::atomic<unsigned>    m_Size;
+    };
+
+    struct BlockIterator
+    {
+        BlockIterator(Block* a_Block) : m_Block(a_Block)
+        {
+            m_Index = (m_Block && (m_Block->m_Size > 0)) ? 0 : -1;
+        }
+
+        T& operator*()
+        {
+            return m_Block->m_Data[m_Index];
+        }
+
+        bool operator!=(const BlockIterator & other) const
+        {
+            bool returnValue = m_Index != other.m_Index;
+            return returnValue;
+        }
+
+        BlockIterator& operator++()
+        {
+            if (++m_Index == m_Block->m_Size)
+            {
+                if (m_Block->m_Next && m_Block->m_Next->m_Size > 0)
+                {
+                    m_Index = 0;
+                    m_Block = m_Block->m_Next;
+                }
+                else
+                {
+                    m_Index = -1;
+                }
+            }
+
+            return *this;
+        }
+
+        Block*      m_Block;
+        unsigned    m_Index;
+    };
+
+
     BlockChain() : m_NumBlocks(1), m_NumItems(0)
     {
-        m_Root = m_Current = new Block<T, BlockSize>(this, nullptr);
+        m_Root = m_Current = new Block(this, nullptr);
     }
 
     ~BlockChain()
@@ -105,7 +102,7 @@ template < class T, int BlockSize > struct BlockChain
         while( m_Current->m_Next )
             m_Current = m_Current->m_Next;
 
-        Block<T, BlockSize>* prev = m_Current;
+        Block* prev = m_Current;
         while (prev)
         {
             prev = m_Current->m_Prev;
@@ -119,15 +116,15 @@ template < class T, int BlockSize > struct BlockChain
         m_Current->Add(a_Item);
     }
 
-    void push_back( const T* a_Array, unsigned int a_Num )
+    void push_back( const T* a_Array, unsigned a_Num )
     {
-        for( unsigned int i = 0; i < a_Num; ++i )
+        for( unsigned i = 0; i < a_Num; ++i )
             m_Current->Add( a_Array[i] );
     }
 
-    void push_back_n( const T & a_Item, unsigned int a_Num )
+    void push_back_n( const T & a_Item, unsigned a_Num )
     {
-        for( unsigned int i = 0; i < a_Num; ++i )
+        for( unsigned i = 0; i < a_Num; ++i )
             m_Current->Add( a_Item );
     }
 
@@ -138,7 +135,7 @@ template < class T, int BlockSize > struct BlockChain
         m_NumItems     = 0;
         m_NumBlocks    = 1;
 
-        Block<T, BlockSize>* prev = m_Current;
+        Block* prev = m_Current;
         while (prev!=m_Root)
         {
             prev = m_Current->m_Prev;
@@ -151,7 +148,7 @@ template < class T, int BlockSize > struct BlockChain
 
     void Reset()
     {
-        Block<T, BlockSize>* blockPtr = m_Root;
+        Block* blockPtr = m_Root;
         while( blockPtr )
         {
             blockPtr->m_Size = 0;
@@ -163,7 +160,7 @@ template < class T, int BlockSize > struct BlockChain
         m_Current = m_Root;
     }
 
-    bool keep( int a_MaxElems )
+    bool keep(unsigned a_MaxElems )
     {
         bool hasDeleted = false;
         a_MaxElems = max( BlockSize + 1, a_MaxElems );
@@ -186,32 +183,32 @@ template < class T, int BlockSize > struct BlockChain
         return hasDeleted;
     }
 
-    int size() const { return m_NumItems; }
+    unsigned size() const { return m_NumItems; }
     
-    T* SlowAt( int a_Index )
+    T* SlowAt(unsigned a_Index )
     {
         if( a_Index < m_NumItems && a_Index >= 0 )
         {
-            int count = 1;
-            Block<T, BlockSize>* block = m_Root;
+            unsigned count = 1;
+            Block* block = m_Root;
             while( count * BlockSize < a_Index && block && block->m_Next )
             {
                 block = block->m_Next;
                 ++count;
             }
 
-            int index = a_Index % BlockSize;
+            unsigned index = a_Index % BlockSize;
             return &block->m_Data[index];
         }
 
         return nullptr;
     }
-    
-    BlockIterator<T, BlockSize> begin(){ return BlockIterator<T, BlockSize>(m_Root); }
-    BlockIterator<T, BlockSize> end(){ return BlockIterator<T, BlockSize>(nullptr); }
+   
+    BlockIterator begin(){ return BlockIterator(m_Root); }
+    BlockIterator end(){ return BlockIterator(nullptr); }
 
-    Block<T, BlockSize>* m_Root;
-    Block<T, BlockSize>* m_Current;
-    atomic<int>          m_NumBlocks;
-    atomic<int>          m_NumItems;
+    Block*      m_Root;
+    Block*      m_Current;
+    std::atomic<unsigned> m_NumBlocks;
+    std::atomic<unsigned> m_NumItems;
 };
