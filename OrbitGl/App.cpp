e0c398b71dbe3fa31ed957fa19637ca724232ac8
Daniel Olivier <dan_olivier@hotmail.com>
2017-10-19 00:15:36 -0400

    Remove dependency of asio on boost

--- OrbitGl/App.cpp
+++ OrbitGl/App.cpp
@@ -395,7 +395,7 @@ void OrbitApp::CallHome()
 void OrbitApp::CallHomeThread()
 {
     asio::ip::tcp::iostream stream;
-    stream.expires_from_now( boost::posix_time::seconds( 60 ) );
+    stream.expires_from_now( chrono::seconds( 60 ) );
 
     const bool isLocal = false;
 
